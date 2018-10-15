4a0325f070ca1afe18370fcf6b3f9202bde56e47
Jason Bryer <jason@bryer.org>
2018-10-15 19:42:34 -0400

    Added instructions for loading homework data.

--- R/2018-10-15.R
+++ R/2018-10-15.R
@@ -8,3 +8,13 @@ shiny_demo('CLT_mean')
 startLab('Lab4a')
 # Click Lab4a in the File browser
 # Then click More -> Set As Working Directory
+
+# Data for the homework
+# Download from here: https://github.com/jbryer/DATA606Fall2018/tree/master/data/openintro.org
+install.packages(c('openintro')) # Only necessary once to install the package
+library(openintro)
+data(package='openintro') # Lists all the datasets in this package
+# Here is a mapping of problems to dataset: https://github.com/jbryer/DATA606Fall2018/blob/master/data/openintro.org/Ch%204%20Exercise%20Data/000%20-%20CH%204%20GUIDE.txt
+# See, for example, problem 4.14 needs ageAtMar. Use the data command to load as follows:
+data("ageAtMar") # for problem 4.14
+
