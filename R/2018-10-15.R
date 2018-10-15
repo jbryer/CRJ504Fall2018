616aa6e0598e9154f5f58e61aab676eb9e1d9704
Jason Bryer <jason@bryer.org>
2018-10-15 19:30:16 -0400

    Updated R script from class.

--- R/2018-10-15.R
+++ R/2018-10-15.R
@@ -1,2 +1,10 @@
 # R Notes from October 15th class
+library(DATA606)
 
+shiny_demo('sampdist')
+shiny_demo('CLT_mean')
+
+# Starting Lab 4a
+startLab('Lab4a')
+# Click Lab4a in the File browser
+# Then click More -> Set As Working Directory
