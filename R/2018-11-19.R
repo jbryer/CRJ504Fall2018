d440bfad5d464057895fe001ead61cc34d5c1815
Jason Bryer <jason@bryer.org>
2018-11-19 19:17:13 -0500

    Added R script

---
+++ R/2018-11-19.R
@@ -0,0 +1,7 @@
+library(DATA606)
+
+# Make sure the Files tab is where you want the lab folder to go
+# Click More -> Set as Working Directory
+startLab('Lab7')
+# Click Lab 7 in the Files tab
+# Click More -> Set as Working Directory
